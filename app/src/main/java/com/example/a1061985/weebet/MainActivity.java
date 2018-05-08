5e072767319acae55cdc3df0b308cb545e0f6bca
T. Andersen <Tzarreth@users.noreply.github.com>
2018-05-09 00:04:41 +0200

    UI Navigation
    Soccer button working for navigation

--- app/src/main/java/com/example/a1061985/weebet/MainActivity.java
+++ app/src/main/java/com/example/a1061985/weebet/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.a1061985.weebet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends MenuActivity {
+
+    public Button soccerButton;
+
+    public void onSoccerButtonPress(){
+        soccerButton = (Button)findViewById(R.id.soccerButton);
+        soccerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CompetitionsActivity.class);
+                intent.putExtra("sportId", 1);
+                startActivity(intent);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        onSoccerButtonPress();
     }
 }
